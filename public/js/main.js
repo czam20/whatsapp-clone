2e336d95f400947f097e53132491b739d2de6e9d
czam20 <ccza2010@gmail.com>
2022-02-20 20:51:03 -0400

    Modified styles of profile details

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,18 @@
 const user_profile = document.querySelector("#user-profile")
-user_profile.onclick = () => {
-    document.querySelector("#user-profile-details").classList.remove("hidden")
-    document.querySelector("#preview").classList.add("hidden")
-}
+user_profile.onclick = () => document.querySelector("#user-profile-details").classList.add("show")
+
 
 const back = document.querySelector("#back")
-back.onclick = () => {
-    document.querySelector("#preview").classList.remove("hidden")
-    document.querySelector("#user-profile-details").classList.add("hidden")
-}
+back.onclick = () => document.querySelector("#user-profile-details").classList.remove("show")
 
 function edit_profile_info(info, btn) {
+    info.classList.toggle("active")
+
     if (info.hasAttribute("disabled")) {
         info.removeAttribute("disabled")
-        info.style.borderBottom = "2px solid rgb(159, 12, 196)"
         btn.innerHTML = `<i class="fas fa-check"></i>`
     } else {
         info.setAttribute("disabled", "disabled")
-        info.style.borderBottom = "none"
         btn.innerHTML = `<i class="fas fa-pen"></i>`
     }
 }
@@ -27,3 +22,6 @@ edit_name.onclick = () => edit_profile_info(document.querySelector("#edit-name")
 
 const edit_info = document.querySelector("#edit-info-btn")
 edit_info.onclick = () => edit_profile_info(document.querySelector("#edit-info"), edit_info)
+
+const chat = document.querySelector(".chat-preview")
+chat.onclick = () => document.querySelector(".chat-container").classList.toggle("active")
